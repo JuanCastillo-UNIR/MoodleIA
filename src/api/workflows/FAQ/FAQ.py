143fa9ceb3037b7dc648325d862baadbc9d5ff70
JuanCastillo-UNIR <juanmanuel.castillo@unir.net>
2024-05-27 16:23:56 -0500

    pruebas generales

---
+++ src/api/workflows/FAQ/FAQ.py
@@ -0,0 +1,32 @@
+import requests  
+from bs4 import BeautifulSoup, Tag  
+from IPython.display import display, HTML  
+  
+# Obtener y analizar el contenido HTML  
+soup = BeautifulSoup(requests.get('https://becat.online/FAQ/').content, 'html.parser') 
+
+# Procesar y mostrar el contenido de cada acordeón  
+for accordion in soup.select('#accordion h3'):  
+    title = accordion.get_text(strip=True)  
+    content_div = accordion.find_next_sibling('div')  
+    content = ""  
+  
+    # Función para asegurar que todas las URLs sean absolutas  
+    def ensure_absolute_url(url):  
+        return url if url.startswith('http') else f"https://becat.online/FAQ/{url}"  
+  
+    # Procesar todos los elementos dentro del div de contenido  
+    for element in content_div.children:  
+        if isinstance(element, Tag):  # Asegura que el elemento es un Tag antes de usar find_all  
+            imgs = element.find_all('img')  
+            for img in imgs:  
+                img['src'] = ensure_absolute_url(img['src'])  
+  
+            links = element.find_all('a')  
+            for link in links:  
+                if 'href' in link.attrs:  
+                    link['href'] = ensure_absolute_url(link['href'])  
+  
+            content += str(element)  
+  
+    display(HTML(f"<h2>{title}</h2><div>{content}</div>")) 
