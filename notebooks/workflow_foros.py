f89c8dd3b6fc9c799f61641d2959de9fac730956
Oscar Andres Pinilla Leon <oscarandres.pinilla@unir.net>
2024-06-06 16:19:33 -0500

    Se realizaron modificaciones en el workflow para Foros

--- notebooks/workflow_foros.py
+++ notebooks/workflow_foros.py
@@ -12,19 +12,6 @@ import PyPDF2
 from src.api.common.dependency_container import DependencyContainer
 
 
-# class ForosWorkflow:
-#     _openai_service: GptContent
-    
-#     def __init__(self,
-#         openai_service: OpenAIService, 
-#         prompt_service: PromptService
-#     ) -> None:
-#         self._openai_service = GptContent(openai_service, prompt_service)
-
-#     def execute(self, request: SchemaRequest) -> dict: # type: ignore
-#         return self._openai_service.get_foros_content(str(request))
-
-
 class ForosWorkflow:
     _openai_service: GptContent
     
